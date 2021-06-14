dd1bf69ef641f46b88adefe2994d3193e085ac1c
HenricoW <henrico.ashley.White@gmail.com>
2021-06-14 16:19:38 +0200

    404 page

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Page404 = () => {
+    return (
+        <section className="hero-section">
+            <div className="container">
+                <div className="left-col">
+                    <h1>Error 404</h1>
+                    <p>The page you requested was not found</p>
+                </div>
+            </div>
+        </section>
+    );
+};
