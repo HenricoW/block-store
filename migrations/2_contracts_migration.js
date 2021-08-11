38732b3c63e5efe15744ec3146ac7e76ce39a29f
HenricoW <henrico.ashley.White@gmail.com>
2021-08-11 20:35:50 +0200

    config and deployment on Rinkeby

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -3,25 +3,36 @@ const W3Shop = artifacts.require("Web3Shop");
 const SToken = artifacts.require("ShopToken");
 const NFTcontr = artifacts.require("W3StoreProduct");
 
-module.exports = async function (deployer, _network, accounts) {
+rinkebyUSDC = "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b";
+
+module.exports = async function (deployer, network, accounts) {
     const [admin, cust1, cust2, _] = accounts;
 
-    const NFTapiEndpoint = "https://totally-legit-url.com/nft/";
+    const NFTapiEndpoint = process.env.REACT_APP_FB_API_URL + "/nft/";
 
-    await deployer.deploy(MockUSD, { from: admin });
-    const mUSD = await MockUSD.deployed();
+    let shopCurrencyAddr;
+    switch (network) {
+        case "development": {
+            await deployer.deploy(MockUSD, { from: admin });
+            const mUSD = await MockUSD.deployed();
 
-    await deployer.deploy(W3Shop, mUSD.address, NFTapiEndpoint, { from: admin });
+            await mUSD.faucet(web3.utils.toWei("1000"), { from: cust1 });
+            await mUSD.faucet(web3.utils.toWei("300"), { from: cust2 });
+            await mUSD.faucet(web3.utils.toWei("100"), { from: admin });
+
+            shopCurrencyAddr = mUSD.address;
+        }
+        case "rinkeby":
+            shopCurrencyAddr = rinkebyUSDC;
+    }
+
+    // deploy the main Shop contract, specifying the shop currency & the NFT api endpoint
+    // NFT contract is deployed by the Shop contract
+    await deployer.deploy(W3Shop, shopCurrencyAddr, NFTapiEndpoint, { from: admin });
     const w3shop = await W3Shop.deployed();
 
+    // deploy the shop reward token, specifying the Shop contract address
     await deployer.deploy(SToken, w3shop.address, { from: admin });
     const stkn = await SToken.deployed();
-
-    await deployer.deploy(NFTcontr, "A", "B", "C", { from: admin }); // to have available in Truffle CLI
-
     await w3shop.updateShopToken(stkn.address, { from: admin });
-
-    await mUSD.faucet(web3.utils.toWei("1000"), { from: cust1 });
-    await mUSD.faucet(web3.utils.toWei("300"), { from: cust2 });
-    await mUSD.faucet(web3.utils.toWei("100"), { from: admin });
 };
--- truffle-config.js
+++ truffle-config.js
@@ -17,12 +17,14 @@
  * phrase from a file you've .gitignored so it doesn't accidentally become public.
  *
  */
+require("dotenv").config();
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraKey = process.env.REACT_APP_INFURA_ID;
+console.log(process.env.REACT_APP_INFURA_ID);
+
+const pKey = process.env.PKEY;
+console.log(process.env.PKEY);
 
 module.exports = {
     /**
@@ -44,11 +46,11 @@ module.exports = {
         // tab if you use this network and you must also set the `host`, `port` and `network_id`
         // options below to some value.
         //
-        // development: {
-        //  host: "127.0.0.1",     // Localhost (default: none)
-        //  port: 8545,            // Standard Ethereum port (default: none)
-        //  network_id: "*",       // Any network (default: none)
-        // },
+        development: {
+            host: "127.0.0.1", // Localhost (default: none)
+            port: 9545, // Standard Ethereum port (default: none)
+            network_id: "*", // Any network (default: none)
+        },
         // Another network with more advanced options...
         // advanced: {
         // port: 8777,             // Custom port
@@ -60,14 +62,18 @@ module.exports = {
         // },
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        rinkeby: {
+            provider: () =>
+                new HDWalletProvider({
+                    privateKeys: [pKey],
+                    providerOrUrl: `https://rinkeby.infura.io/v3/${infuraKey}`,
+                }),
+            network_id: 4, // rinkeby's id
+            gas: 10000000, // rinkeby has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+        },
         // Useful for private networks
         // private: {
         // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
