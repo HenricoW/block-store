d92bbb48d92e84f65923659c4b873ed6062f0d68
HenricoW <henrico.ashley.White@gmail.com>
2021-09-28 19:21:23 +0200

    opensea compatibility (& prettier)

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -6,116 +6,117 @@ import { selectProduct } from "../redux/actions/currentProductActions";
 const nftEndpoint = process.env.REACT_APP_FB_API_URL + "/nft";
 
 export const ProductDetailPage = ({ web3, contracts, accounts, web3connect }) => {
-    const { product_id } = useParams();
+  const { product_id } = useParams();
 
-    const products = useSelector((state) => state.allProducts.products);
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const qty = useRef(null);
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const qty = useRef(null);
 
-    useEffect(() => {
-        if (products.length < 1) history.push("/products");
-        else qty.current.value = 1;
-        dispatch(selectProduct(product_id));
-        window.scroll(0, 0);
-    }, [products.length, product_id, dispatch, history]);
+  useEffect(() => {
+    if (products.length < 1) history.push("/products");
+    else qty.current.value = 1;
+    dispatch(selectProduct(product_id));
+    window.scroll(0, 0);
+  }, [products.length, product_id, dispatch, history]);
 
-    const product = useSelector((state) => state.allProducts.currentItem);
+  const product = useSelector((state) => state.allProducts.currentItem);
 
-    const handleMMError = (err) => {
-        if (err.message.includes("User denied transaction")) {
-            alert("You rejected the transaction"); // change to modal
-        }
-        console.log(err.message);
+  const handleMMError = (err) => {
+    if (err.message.includes("User denied transaction")) {
+      alert("You rejected the transaction"); // change to modal
+    }
+    console.log(err.message);
+  };
+
+  // depends on: web3, contracts, accounts, nftEndpoint, handleMMError
+  const onBuy = async (qty, product) => {
+    if (web3 === undefined || contracts.USDcontr === undefined || contracts.storeContr === undefined) {
+      web3connect();
+      return;
+    }
+
+    if (!product.id) {
+      const err = new Error("Selected item ID not set");
+      console.log(err);
+      return;
+    }
+
+    const amount = web3.utils.toWei((qty * product.price).toString(), "mwei");
+    console.log(amount.toString());
+    const currAcc = accounts[0];
+
+    // set the approval
+    try {
+      await contracts.USDcontr.methods.approve(contracts.storeContr.options.address, amount).send({ from: currAcc });
+    } catch (err) {
+      console.log(new Error("Approval failed"));
+      handleMMError(err);
+      return;
+    }
+
+    const nftData = {
+      name: product.title,
+      description: product.desc,
+      image: product.imageUrl,
+      itemID: product.id,
     };
 
-    // depends on: web3, contracts, accounts, nftEndpoint, handleMMError
-    const onBuy = async (qty, product) => {
-        if (web3 === undefined || contracts.USDcontr === undefined || contracts.storeContr === undefined) {
-            web3connect();
-            return;
-        }
-
-        if (!product.id) {
-            const err = new Error("Selected item ID not set");
-            console.log(err);
-            return;
-        }
-
-        const amount = web3.utils.toWei((qty * product.price).toString(), "mwei");
-        console.log(amount.toString());
-        const currAcc = accounts[0];
-
-        // set the approval
-        try {
-            await contracts.USDcontr.methods
-                .approve(contracts.storeContr.options.address, amount)
-                .send({ from: currAcc });
-        } catch (err) {
-            console.log(new Error("Approval failed"));
-            handleMMError(err);
-            return;
-        }
-
-        const nftData = {
-            title: product.title,
-            desc: product.desc,
-            imageUrl: product.imageUrl,
-            itemID: product.id,
-        };
-
-        const fetchOptions = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        };
-
-        // cunduct the purchase
-        let tokenID;
-        contracts.storeContr.methods
-            .purchase(amount, product.id)
-            .send({ from: currAcc })
-            .then((receipt) => {
-                tokenID = receipt.events.nftMinted.returnValues.nftID;
-                return fetch(nftEndpoint, {
-                    ...fetchOptions,
-                    body: JSON.stringify({ ...nftData, tokenID }),
-                });
-            })
-            .then((resp) => resp.json())
-            .then(() => console.log("NFT data successfully written"))
-            .catch((err) => {
-                console.log(new Error("Transaction failed"));
-                handleMMError(err);
-            });
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onBuy(qty.current.value, product);
-    };
+    // cunduct the purchase
+    let tokenID;
+    contracts.storeContr.methods
+      .purchase(amount, product.id)
+      .send({ from: currAcc })
+      .then((receipt) => {
+        tokenID = receipt.events.nftMinted.returnValues.nftID;
+        return fetch(nftEndpoint, {
+          ...fetchOptions,
+          body: JSON.stringify({ ...nftData, tokenID }),
+        });
+      })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        console.log("NFT data successfully written");
+      })
+      .catch((err) => {
+        console.log(new Error("Transaction failed"));
+        handleMMError(err);
+      });
+  };
 
-    return product ? (
-        <section className="product-detail">
-            <div className="container">
-                <div className="detail-grid">
-                    <div className="product-image">
-                        <img src={`${product.imageUrl}`} alt={product.title} />
-                    </div>
-                    <div className="product-text">
-                        <h2>{product.title}</h2>
-                        <h3>$ {product.price}</h3>
-                        <p>{product.desc}</p>
-                        <form onSubmit={handleSubmit}>
-                            <input type="number" name="qty" id="qty" ref={qty} min={1} max={10} />
-                            <button>Buy Now</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-    ) : (
-        <div>Loading...</div>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onBuy(qty.current.value, product);
+  };
+
+  return product ? (
+    <section className="product-detail">
+      <div className="container">
+        <div className="detail-grid">
+          <div className="product-image">
+            <img src={`${product.imageUrl}`} alt={product.title} />
+          </div>
+          <div className="product-text">
+            <h2>{product.title}</h2>
+            <h3>$ {product.price}</h3>
+            <p>{product.desc}</p>
+            <form onSubmit={handleSubmit}>
+              <input type="number" name="qty" id="qty" ref={qty} min={1} max={10} />
+              <button>Buy Now</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  ) : (
+    <div>Loading...</div>
+  );
 };
